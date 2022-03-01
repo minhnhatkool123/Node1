8dbd10e884a5067c805021389241e7aa6ad62bd7
Nguyen Pham Minh Nhat <nhatnpm@MECORP.LOCAL>
2022-03-01 16:07:14 +0700

    Node1

--- services/helpers/signJwt.helper.js
+++ services/helpers/signJwt.helper.js
@@ -1,7 +1,6 @@
 const JsonWebToken = require('jsonwebtoken');
 
 module.exports = function (payload, expiresIn) {
-    // /console.log(process.env.ACCESS_TOKEN_SECRET)
     const accessToken = JsonWebToken.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: expiresIn,
     });
